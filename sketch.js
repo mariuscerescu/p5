00eb37d3d81b20782d1d0aad3f6c5444d71271dd
Cerescu Marius <marius.cerescu@gmail.com>
2022-01-27 18:05:03 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(700, 700);
+}
+
+function draw() {
+  background(255);
+  fill(200, 200, 200);
+  rect(250, 180, 75, 300);
+
+
+  fill(150, 255, 150);
+  circle(285, 180, 200);
+}
+
+
+
